46a8692b84e0aa5787d9df3a2ef334eaf9237bbb
Perry Kankam <perrykankam@gmail.com>
2025-03-31 17:26:02 -0400

    promo banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import ButtonSignin from './ButtonSignin';
 import LanguageSelector from './LanguageSelector';
+import PromoBanner from './PromoBanner';
 import { useLocalization } from '@/contexts/LocalizationContext';
 import logo from '@/app/icon.png';
 import config from '@/config';
@@ -50,80 +51,19 @@ const Header = () => {
   }, [searchParams]);
 
   return (
-    <header className='bg-base-200'>
-      <nav
-        className='container flex items-center justify-between px-8 py-4 mx-auto'
-        aria-label='Global'
-      >
-        {/* Your logo/name on large screens */}
-        <div className='flex lg:flex-1'>
-          <Link
-            className='flex items-center gap-2 shrink-0 '
-            href='/'
-            title={`${config.appName} homepage`}
-          >
-            <Image
-              src={logo}
-              alt={`${config.appName} logo`}
-              className='w-28 sm:w-32 md:w-32 lg:w-36 h-auto'
-              placeholder='blur'
-              priority={true}
-            />
-            {/* <span className='font-extrabold text-lg'>{config.appName}</span> */}
-          </Link>
-        </div>
-        {/* Burger button to open menu on mobile */}
-        <div className='flex lg:hidden'>
-          <button
-            type='button'
-            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5'
-            onClick={() => setIsOpen(true)}
-          >
-            <span className='sr-only'>Open main menu</span>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='w-6 h-6 text-base-content'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-              />
-            </svg>
-          </button>
-        </div>
-
-        {/* Your links on large screens */}
-        <div className='hidden lg:flex lg:justify-center lg:gap-12 lg:items-center'>
-          {links.map((link) => (
-            <Link href={link.href} key={link.href} className='link link-hover' title={link.label}>
-              {link.label}
-            </Link>
-          ))}
-        </div>
-
-        {/* CTA and Language Selector on large screens */}
-        <div className='hidden lg:flex lg:justify-end lg:flex-1 lg:items-center lg:gap-4'>
-          <LanguageSelector currentRegion={currentRegion} onRegionChange={setCurrentRegion} />
-          {cta}
-        </div>
-      </nav>
-
-      {/* Mobile menu, show/hide based on menu state. */}
-      <div className={`relative z-50 ${isOpen ? '' : 'hidden'}`}>
-        <div
-          className={`fixed inset-y-0 right-0 z-10 w-full px-8 py-4 overflow-y-auto bg-base-200 sm:max-w-sm sm:ring-1 sm:ring-neutral/10 transform origin-right transition ease-in-out duration-300`}
+    <>
+      <PromoBanner promoCode='SUMMER2024' message='Get 20% off your first purchase with code' />
+      <header className='bg-base-200'>
+        <nav
+          className='container flex items-center justify-between px-8 py-4 mx-auto'
+          aria-label='Global'
         >
-          {/* Your logo/name on small screens */}
-          <div className='flex items-center justify-between'>
+          {/* Your logo/name on large screens */}
+          <div className='flex lg:flex-1'>
             <Link
               className='flex items-center gap-2 shrink-0 '
-              title={`${config.appName} homepage`}
               href='/'
+              title={`${config.appName} homepage`}
             >
               <Image
                 src={logo}
@@ -134,51 +74,115 @@ const Header = () => {
               />
               {/* <span className='font-extrabold text-lg'>{config.appName}</span> */}
             </Link>
+          </div>
+          {/* Burger button to open menu on mobile */}
+          <div className='flex lg:hidden'>
             <button
               type='button'
-              className='-m-2.5 rounded-md p-2.5'
-              onClick={() => setIsOpen(false)}
+              className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5'
+              onClick={() => setIsOpen(true)}
             >
-              <span className='sr-only'>Close menu</span>
+              <span className='sr-only'>Open main menu</span>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
                 viewBox='0 0 24 24'
                 strokeWidth={1.5}
                 stroke='currentColor'
-                className='w-6 h-6'
+                className='w-6 h-6 text-base-content'
               >
-                <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+                />
               </svg>
             </button>
           </div>
 
-          {/* Your links on small screens */}
-          <div className='flow-root mt-6'>
-            <div className='py-4'>
-              <div className='flex flex-col gap-y-4 items-start'>
-                {links.map((link) => (
-                  <Link
-                    href={link.href}
-                    key={link.href}
-                    className='link link-hover'
-                    title={link.label}
-                  >
-                    {link.label}
-                  </Link>
-                ))}
-              </div>
+          {/* Your links on large screens */}
+          <div className='hidden lg:flex lg:justify-center lg:gap-12 lg:items-center'>
+            {links.map((link) => (
+              <Link href={link.href} key={link.href} className='link link-hover' title={link.label}>
+                {link.label}
+              </Link>
+            ))}
+          </div>
+
+          {/* CTA and Language Selector on large screens */}
+          <div className='hidden lg:flex lg:justify-end lg:flex-1 lg:items-center lg:gap-4'>
+            <LanguageSelector currentRegion={currentRegion} onRegionChange={setCurrentRegion} />
+            {cta}
+          </div>
+        </nav>
+
+        {/* Mobile menu, show/hide based on menu state. */}
+        <div className={`relative z-50 ${isOpen ? '' : 'hidden'}`}>
+          <div
+            className={`fixed inset-y-0 right-0 z-10 w-full px-8 py-4 overflow-y-auto bg-base-200 sm:max-w-sm sm:ring-1 sm:ring-neutral/10 transform origin-right transition ease-in-out duration-300`}
+          >
+            {/* Your logo/name on small screens */}
+            <div className='flex items-center justify-between'>
+              <Link
+                className='flex items-center gap-2 shrink-0 '
+                title={`${config.appName} homepage`}
+                href='/'
+              >
+                <Image
+                  src={logo}
+                  alt={`${config.appName} logo`}
+                  className='w-28 sm:w-32 md:w-32 lg:w-36 h-auto'
+                  placeholder='blur'
+                  priority={true}
+                />
+                {/* <span className='font-extrabold text-lg'>{config.appName}</span> */}
+              </Link>
+              <button
+                type='button'
+                className='-m-2.5 rounded-md p-2.5'
+                onClick={() => setIsOpen(false)}
+              >
+                <span className='sr-only'>Close menu</span>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-6 h-6'
+                >
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+                </svg>
+              </button>
             </div>
-            <div className='divider'></div>
-            {/* Your CTA and Language Selector on small screens */}
-            <div className='flex flex-col gap-4'>
-              <LanguageSelector currentRegion={currentRegion} onRegionChange={setCurrentRegion} />
-              {cta}
+
+            {/* Your links on small screens */}
+            <div className='flow-root mt-6'>
+              <div className='py-4'>
+                <div className='flex flex-col gap-y-4 items-start'>
+                  {links.map((link) => (
+                    <Link
+                      href={link.href}
+                      key={link.href}
+                      className='link link-hover'
+                      title={link.label}
+                    >
+                      {link.label}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+              <div className='divider'></div>
+              {/* Your CTA and Language Selector on small screens */}
+              <div className='flex flex-col gap-4'>
+                <LanguageSelector currentRegion={currentRegion} onRegionChange={setCurrentRegion} />
+                {cta}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
