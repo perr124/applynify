471134b14a1167661cce66a91662c1902230f789
Perry Kankam <perrykankam@gmail.com>
2025-03-31 15:03:36 -0400

    additional information placeholder

--- app/dashboard/update-preferences/page.tsx
+++ app/dashboard/update-preferences/page.tsx
@@ -843,8 +843,8 @@ export default function UpdatePreferences() {
                 </label>
                 <textarea
                   className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm'
-                  rows={4}
-                  placeholder='Any other important information we should know about'
+                  rows={5}
+                  placeholder='This could include any additional languages you speak, job roles you’ve recently applied to (please note that we only apply to newly posted jobs within the last 7–10 days), or any other important information we should know.'
                   value={formData.availability.additionalInfo}
                   onChange={(e) => updateFormData('availability', 'additionalInfo', e.target.value)}
                 />
