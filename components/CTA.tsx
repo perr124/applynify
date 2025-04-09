8b304e4052284d0a39e132099cd21f0a18462012
Perry Kankam <perrykankam@gmail.com>
2025-04-08 23:44:11 -0400

    update cta image1

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,16 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import config from '@/config';
+import ctaImage from '@/app/pexels-ron-lach-9832697.jpg';
 
 const CTA = () => {
   return (
     <section className='relative hero overflow-hidden min-h-screen'>
-      <Image
-        src='https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80'
-        alt='Background'
-        className='object-cover w-full'
-        fill
-      />
+      <Image src={ctaImage} alt='Background' className='object-cover w-full' fill />
       <div className='relative hero-overlay bg-neutral bg-opacity-70'></div>
       <div className='relative hero-content text-center text-neutral-content p-8'>
         <div className='flex flex-col items-center max-w-xl p-8 md:p-0'>
