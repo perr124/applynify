6343469c72a3318ee482aa8e0d981dd83cd7f5c4
Perry Kankam <perrykankam@gmail.com>
2025-11-25 10:11:55 -0500

    update tiktok pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
               ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
               
-              ttq.load('D4IRIABC77UAP3H8RKE0');
+              ttq.load('D4ISE0RC77UAP3H8RL8G');
               ttq.page();
             }(window, document, 'ttq');
           `}
